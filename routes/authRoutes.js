64013c2238ee3d20aba294c9efb975a358c7601c
Vorniqo_sol123 <vorniqosolution@gmail.com>
2025-07-18 15:40:49 +0500

    all deleted

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, signup } = require("../controller/authController");
-
-router.post("/login", login);
-router.post("/signup", signup); // Receptionist only
-
-module.exports = router;
