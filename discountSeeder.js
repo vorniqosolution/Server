12a25a91a4c2a2ca5649457b890d259966989f85
Vorniqo_sol123 <vorniqosolution@gmail.com>
2025-07-22 18:20:35 +0500

    feat: add discount seeder script to initialize discount data in the database

---
+++ discountSeeder.js
@@ -0,0 +1,50 @@
+// discountSeeder.js
+// Run this script to seed initial discount data into the database
+
+require('dotenv').config();
+const connectDB = require('./config/db');
+const Discount = require('./model/discount');
+const User = require('./model/User');
+
+const seedDiscounts = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB();
+
+    // Check if discounts already exist
+    const existingDiscounts = await Discount.countDocuments();
+    if (existingDiscounts > 0) {
+      console.log('Discounts already exist in the database');
+      process.exit();
+    }
+
+    // Find an admin user to use as createdBy
+    const adminUser = await User.findOne({ role: 'admin' });
+    if (!adminUser) {
+      console.log('No admin user found. Please run the admin seeder first.');
+      process.exit(1);
+    }
+
+    // Sample discount data
+    const discountData = [
+      {
+        title: "Summer Sale",
+        percentage: 25,
+        startDate: new Date('2025-07-01'),
+        endDate: new Date('2025-10-31'),
+        createdBy: adminUser._id
+      }
+    ];
+
+    // Insert discounts
+    await Discount.insertMany(discountData);
+    console.log('Discount data seeded successfully');
+    console.log(`${discountData.length} discounts created`);
+    process.exit();
+  } catch (err) {
+    console.error('Error seeding discount data:', err);
+    process.exit(1);
+  }
+};
+
+seedDiscounts();
