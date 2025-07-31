0b9e177f9d60c57ba9e072ea7ac6fafd87f29bb6
Vorniqo_sol123 <vorniqosolution@gmail.com>
2025-07-31 21:00:09 +0500

    Refactor code structure for improved readability and maintainability

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -4,17 +4,48 @@ const authenticate = require("../middleware/authMiddleware");
 const invoiceController = require("../controller/invoiceController");
 const authorize = require("../middleware/adminMiddleware"); // Assuming you have this for roles
 
-// Protect all invoice routes
 router.use(authenticate);
 
-router.get("/search/:id", invoiceController.searchInvoices);
+router.get(
+  "/get-all-invoices",
+  authorize("admin"),
+  invoiceController.getAllInvoices
+);
 
-router.get("/:id", invoiceController.getInvoiceById);
+router.get(
+  "/search-Invoices",
+  authorize("admin"),
+  invoiceController.searchInvoices
+);
+
+router.get(
+  "/get-Invoice-By-Id/:id",
+  authorize("admin"),
+  invoiceController.getInvoiceById
+);
+
+router.get(
+  "/:id/download",
+  authorize("receptionist", "admin"),
+  invoiceController.downloadInvoicePdf
+);
 
 router.post(
   "/:id/send-email",
-  authorize("receptionist", "admin"), 
+  authorize("receptionist", "admin"),
   invoiceController.sendInvoiceByEmail
+);
+
+router.patch(
+  "/:id/status",
+  authorize("receptionist", "admin"),
+  invoiceController.updateInvoiceStatus
+);
+
+router.delete(
+  "/delete-Invoice/:id",
+  authorize("admin"),
+  invoiceController.deleteInvoice
 );
 
 module.exports = router;
