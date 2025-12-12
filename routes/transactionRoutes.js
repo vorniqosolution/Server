ca023003afe8266720026eb4384936afb0484f27
DevBasitali <rajaalix2022@gmail.com>
2025-12-12 21:21:46 +0500

    basit

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {addTransaction, getTransactionsBySource} = require("../controller/transactionController");
+const {
+  addTransaction,
+  getTransactions,
+  getDailyCashSummary,
+} = require("../controller/transactionController");
 
 const authenticate = require("../middleware/authMiddleware");
-
 
 router.use(authenticate);
 
@@ -13,6 +16,8 @@ router.post("/add", addTransaction);
 
 // ROUTE: GET /api/transactions
 // Desc:  Get history. Usage: /api/transactions?reservationId=123
-router.get("/get-transactions", getTransactionsBySource);
+// router.get("/get-transactions", getTransactionsBySource);
+router.get("/get-transactions", getTransactions);
+router.get("/get-daily-cash-summary", getDailyCashSummary);
 
 module.exports = router;
